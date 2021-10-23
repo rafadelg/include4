92a22a5db23b6fbf7596bb2e7b68120c047e4bac
Rafa Delgado <rafael.casanova@alu.uhu.es>
2021-10-23 20:28:25 +0200

    come back

---
+++ draftMyWebSite/js/fClave.js
@@ -0,0 +1,33 @@
+﻿
+function comprobarClave() {
+	var verLargo="no";
+	var verIgual="no";
+	var pwd1 = document.formulario.clave1;
+	var pwd2 = document.formulario.clave2;
+	var aviso= document.getElementById("aviso");
+	aviso.innerHTML = "";
+	
+	if (pwd1.value.length >= 6 && pwd2.value.length >= 6) {
+ 		verLargo = "si";
+	}
+	else {
+		texto = "La contraseña debe tener más de 6 caracteres.<br/>";
+		aviso.innerHTML += texto;
+	}
+	if (pwd1.value == pwd2.value) {
+		verIgual = "si";
+	}
+	else {
+		texto = "La contraseña y su repetición deben coincidir.<br/>";
+		aviso.innerHTML += texto;
+	}
+	if (verLargo == "no" || verIgual == "no") {
+		return false;
+	}
+}
+
+function restaurar() {
+	var aviso = document.getElementById("aviso");
+	aviso.innerHTML = "";
+}
+
---
+++ draftMyWebSite/js/fFecha.js
@@ -0,0 +1,31 @@
+﻿var separador=":";
+function hora() {
+	var fechaHora = new Date();
+	var horas = fechaHora.getHours();
+	var minutos = fechaHora.getMinutes();
+	if(horas < 10) { horas = '0' + horas; }
+	if(minutos < 10) { minutos = '0' + minutos; }
+	separador == ":" ? separador = " " : separador = ":";
+	document.getElementById('hora').firstChild.nodeValue = horas+separador+minutos;
+	setTimeout(hora, 1000);
+}
+
+/*
+var separador=":";
+function hora() {
+
+	var fecha = new Date();
+	var hora = fecha.getHours();
+	var minuto = fecha.getMinutes();
+	var meridiano = " am";
+	
+	if(hora > 12){hora -= 12; meridiano = " pm";}
+	//if (hora < 10) {hora = "0" + hora;}
+	if (minuto < 10) {minuto = "0" + minuto;}
+	separador == ":" ? separador = " " : separador = ":";
+	var laHora = hora + separador + minuto + meridiano;
+
+	document.getElementById('hora').firstChild.nodeValue = laHora;
+	tiempo = setTimeout('hora()',1000);
+}
+*/
