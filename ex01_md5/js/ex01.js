b821aa8f0925614e2b8e915ae7a0120b63b5e5c4
Rafa Delgado <rafael.casanova@alu.uhu.es>
2021-10-23 20:12:46 +0200

    ok

--- ex01_md5/js/ex01.js
+++ ex01_md5/js/ex01.js
@@ -1,5 +1,10 @@
 // https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
 
 function md5Conv(formulario){
-    alert("El Hash MD5 es: " + md5(formulario.msg.value));
- }
+    if (formulario.msg.value == null || formulario.msg.value == "") {
+        alert("Has de introducir una cadena");
+    }       
+    else {
+        alert("El Hash MD5 es: " + md5(formulario.msg.value));
+    }
+}
